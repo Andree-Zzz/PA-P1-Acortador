34c10faa45aef89ef210452e15c0ed61e53aae5d
Catalina-Diaz <jcatalinadc@gmail.com>
2022-03-10 21:01:48 -0500

    Centrado y Credenciales BD

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ def generate_short_id(num_of_chars: int):
 
 
 db = mysql.connector.connect(
-    host = 'localhost',
-    user = 'root',
-    password = '',
-    database = 'acortador',
+    host = 'academia.c1mebdhdxytu.us-east-1.rds.amazonaws.com',
+    user = 'p2',
+    password = 'ALrUBIaLYcHR',
+    database = 'p2',
     port = '3306'
 )
 db.autocommit = True # Desactivar el cache
