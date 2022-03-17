32f5943cc2023a0c81a79a15e405e9a8b85b96c7
Andree-Zzz <mad08061@gmail.com>
2022-03-17 01:50:10 -0500

    Rediseñando y agregabdo funcionalidad

--- main.py
+++ main.py
@@ -13,13 +13,24 @@ db = mysql.connector.connect(
     database = 'p2',
     port = '3306'
 )
+# db = mysql.connector.connect(
+#     host = '127.0.0.1',
+#     user = 'root',
+#     password = '',
+#     database = 'acortador',
+#     port = '3306'
+# )
 db.autocommit = True
 
 app = Flask(__name__)
 
 @app.get("/")
 def index():
-    return render_template('index.html')
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM acortador ORDER BY id DESC LIMIT 5")
+    urlItems = cursor.fetchall()
+    cursor.close()
+    return render_template("index.html",urlItems = urlItems)
 
 @app.post("/")
 def crearShort():
@@ -31,7 +42,7 @@ def crearShort():
         url_short,
     ))
     cursor.close()
-    return render_template("index.html",url_short = url_short)
+    return redirect(url_for("index"))
 
 @app.route("/<url_short>")
 def redirect_url(url_short):
@@ -39,6 +50,7 @@ def redirect_url(url_short):
     cursor = db.cursor()
     cursor.execute("SELECT url FROM acortador WHERE url_short = %s", (url_short,))
     url = cursor.fetchone()
+    cursor.close()
     if(url != None): return redirect(url[0])
     return redirect(url_for('index'))
 
